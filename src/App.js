763aca49ad917bb825aa3ef3ae18d510ac1d44af
Aldo Tobing <aldo_tobing@hotmail.com>
2022-10-14 11:06:04 +0700

    comm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-import Dashboard from 'pages/Dashboard'
+// import Dashboard from 'pages/Dashboard'
 // import Register from 'pages/Register'
 // import Login from 'pages/Login'
 import Calculator from 'pages/Calculator'
